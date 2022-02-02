fc22d602fd80cdec587bede64035be3065855656
Prasoon Soni <prasoonsoni03@gmail.com>
2022-02-02 22:55:22 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
