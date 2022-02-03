f3012dc87e357039f82bdc15b1b648cbc331cd16
Prasoon Soni <prasoonsoni03@gmail.com>
2022-02-03 18:01:08 +0530

    Issues Solved

--- routes/links/getlinkbyusername.js
+++ routes/links/getlinkbyusername.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const { links } = require('express/lib/response');
 const router = express.Router();
 const Links = require('../../models/Links');
+const Users = require('../../models/Users');
 router.get('/:username', async(req,res)=>{
     try {
         const username = req.params.username;
+        const user = await Users.findOne({username});
         const links = await Links.findOne({username});
-        if(!links){
+        if(!user){
             return res.status(400).json({success:false, message:"No user available."});
+        }
+        if(links.link.length==0){
+            return res.status(400).json({success:false, message:"No links available."});
         }
         res.status(201).json({success:true,username:username, links:links.links});
     } catch (error) {
