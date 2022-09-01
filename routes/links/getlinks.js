15f9912b0900347b7367e984e1a7c482c2381b40
prasoonsoni <prasoonsoni03@gmail.com>
2022-09-01 23:07:08 +0530

    Solved Link Issue

--- routes/links/getlinks.js
+++ routes/links/getlinks.js
@@ -2,20 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Links = require('../../models/Links');
 const fetchuser = require('../../middleware/fetchuser');
-router.get('/', fetchuser, async(req,res)=>{
+router.get('/', fetchuser, async (req, res) => {
     try {
-        const {id, username} = req.user;
-        const links = await Links.findOne({id, username});
-        if(!links){
-            return res.status(400).json({success:false, message:"No links available"});
-        }
-        if(links.links.length==0){
-            return res.status(400).json({success:false, message:"No links available"});
-        }
-        res.status(201).json({success:true,username:username, links:links.links});
+        const { id, username } = req.user;
+        const links = await Links.findOne({ id, username });
+        res.status(201).json({ success: true, username: username, links: links.links });
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({success:false, message:"Some internal server error occured!!"})
+        res.status(500).json({ success: false, message: "Some internal server error occured!!" })
     }
 });
 
